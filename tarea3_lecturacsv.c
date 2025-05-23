f90fca61434d9fb31eb19429e050faf53c829bc2
Darkbrighto <hugo.gallardo.c@mail.pucv.cl>
2025-05-23 00:09:54 +0000

    Co-authored-by: Grengo-true <Grengo-true@users.noreply.github.com>

--- tarea3_lecturacsv.c
+++ tarea3_lecturacsv.c
@@ -4,9 +4,59 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct Nodo {
+  int id;
+  char *nombre;
+  char *descripcion;
+  List *items; // Lista de items
+  int arriba;
+  int abajo;
+  int izquierda;
+  int derecha;
+  int es_final;
+  struct Nodo* siguiente;
+} Nodo;
+
+typedef struct Grafo{
+  Nodo* nodos;
+  int total_nodos;
+} Grafo;
+
+typedef struct Item{
+  char nombre[100];
+  int valor;
+  int peso;
+} Item;
+/* ptropptied
+
+typedef struct Jugador{
+  char nombre[100];
+  int vida;
+  int oro;
+  List* items; // Lista de items
+} Jugador;
+
+*/
+
+/*## **Menú Principal**
+
+1. **Cargar Laberinto desde Archivo CSV**
+    - Leer un archivo CSV que describe los **escenarios**: sus descripciones, decisiones (con direcciones posibles), ítems disponibles (nombre, peso, valor) y conexiones.
+    - Construir el **grafo (explícito)** a partir de esa información.
+    
+    Archivo de ejemplo: [graphquest.csv](https://drive.google.com/file/d/1Xu8du5BrdFG7hd1GW63gB4F5AgKRMbGZ/view?usp=sharing)
+    
+2. **Iniciar Partida**
+    - Comienza el juego desde el escenario inicial. Se muestra el estado del jugador y el menú de opciones correspondientes.
+*/
+
+
+
+
 /**
  * Carga canciones desde un archivo CSV
  */
+/*
 void leer_escenarios() {
   // Intenta abrir el archivo CSV que contiene datos de películas
   FILE *archivo = fopen("data/graphquest.csv", "r");
@@ -67,10 +117,46 @@ void leer_escenarios() {
   fclose(archivo); // Cierra el archivo después de leer todas las líneas
 
 }
+*/
 
+void inicio_juego()
+{
+  short cargado = 0;
+  while(1)
+  {
+    printf("1. Cargar Laberinto \n");
+    printf("2. Iniciar Partida \n");
+    printf("3. Salir \n");
+    printf("Seleccione una opción: ");
+    int opcion;
+    scanf("%d", &opcion);
+    switch(opcion)
+    {
+      case 1:
+        // leer_escenarios();
+        printf("Laberinto cargado.\n");
+        cargado = 1;
+        break;
+      case 2:
+        if(cargado == 0)
+        {
+          printf("Debe cargar el laberinto primero.\n");
+          break;
+        }
+        // iniciar_partida();
+        break;
+      case 3:
+        printf("saliendo.\n");
+        exit(0);
+      default:
+        printf("Opción no válida. Intente de nuevo.\n");
+    }
+    limpiarPantalla();
+  }
+}
 
 int main() {
-  leer_escenarios();
+  inicio_juego();
 
   return 0;
 }
